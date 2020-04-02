aedfe14d58baccd18b348d11d9362667ef211b6c
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2020-04-02 11:40:34 +0200

    Add initial sort order check
    Applied only to USE flags for now.

--- check_packages.py
+++ check_packages.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
+import operator
+from functools import partial
+from itertools import tee
 from pathlib import Path
 
 import portage
 
 DEFAULT_CHECK_KEYWORDS = True
 DEFAULT_CHECK_LICENSES = True
+DEFAULT_CHECK_SORT_ORDER = True
 DEFAULT_CHECK_USE_FLAGS = True
 DEFAULT_KEYWORD_PATH = Path('/etc/portage/package.accept_keywords')
 DEFAULT_LICENSE_PATH = Path('/etc/portage/package.license')
 DEFAULT_USE_PATH = Path('/etc/portage/package.use')
+
+
+def _is_sorted(iterable, comparator=operator.le):
+    a, b = tee(iterable)
+    next(b, None)
+    return all(map(comparator, a, b))
 
 
 def _get_input_file_paths(path):
@@ -32,6 +42,11 @@ def _parse_args():
                                help="do not check package licenses")
     license_group.add_argument('-L', '--licenses', dest='licenses', action='store_true',
                                help="check package licenses (default)")
+    sort_order_group = parser.add_mutually_exclusive_group()
+    sort_order_group.add_argument('-s', '--no-sort-order', dest='sort_order', action='store_false',
+                                  help="do not check sort order")
+    sort_order_group.add_argument('-S', '--sort-order', dest='sort_order', action='store_true',
+                                  help="check sort order (default)")
     use_flag_group = parser.add_mutually_exclusive_group()
     use_flag_group.add_argument('-u', '--no-use-flags', dest='useflags', action='store_false',
                                 help="do not check package USE flags")
@@ -44,12 +59,12 @@ def _parse_args():
     parser.add_argument('--use-path', metavar='PATH', default=DEFAULT_USE_PATH,
                         help="the path (file or directory) to check for package USE flags (default: %(default)s)")
     parser.set_defaults(keywords=DEFAULT_CHECK_KEYWORDS, licenses=DEFAULT_CHECK_LICENSES,
-                        useflags=DEFAULT_CHECK_USE_FLAGS)
+                        sort_order=DEFAULT_CHECK_SORT_ORDER, useflags=DEFAULT_CHECK_USE_FLAGS)
     return parser.parse_args()
 
 
-def _strip_use_flag(use_flag):
-    return use_flag[1:] if use_flag.startswith(('+', '-')) else use_flag
+def _strip_use_flag(use_flag, prefix=('+', '-')):
+    return use_flag[1:] if use_flag.startswith(prefix) else use_flag
 
 
 def check_keywords(path=DEFAULT_KEYWORD_PATH):
@@ -111,7 +126,7 @@ def check_licenses(path=DEFAULT_LICENSE_PATH):
                     ))
 
 
-def check_use_flags(path=DEFAULT_USE_PATH):
+def check_use_flags(path=DEFAULT_USE_PATH, check_sort_order=DEFAULT_CHECK_SORT_ORDER):
     print("Checking USE flags...")
     for p in _get_input_file_paths(Path(path)):
         with open(p) as f:
@@ -122,7 +137,12 @@ def check_use_flags(path=DEFAULT_USE_PATH):
 
                 tokens = line.split()
                 atom = tokens[0]
-                use_flags = list(map(_strip_use_flag, tokens[1:]))
+                use_flags = tokens[1:]
+                if check_sort_order and not _is_sorted(map(partial(_strip_use_flag, prefix='+'), use_flags)):
+                    print("{file_name}: {atom} has unsorted USE flags!".format(
+                        file_name=portage.output.white(f.name),
+                        atom=portage.output.red(atom),
+                    ))
                 installed_packages = portage.db[portage.root]['vartree'].dbapi.match(atom)
                 if not installed_packages:
                     print("{file_name}: {atom} is no longer installed!".format(
@@ -141,7 +161,7 @@ def check_use_flags(path=DEFAULT_USE_PATH):
                         ))
                     else:
                         available_use_flags = list(map(_strip_use_flag, package_iuse.split()))
-                        for use_flag in use_flags:
+                        for use_flag in map(_strip_use_flag, use_flags):
                             if use_flag not in available_use_flags:
                                 print("{file_name}: {atom} no longer makes use of the {use_flag} USE flag!".format(
                                     file_name=portage.output.white(f.name),
@@ -157,4 +177,4 @@ if __name__ == '__main__':
     if args.licenses:
         check_licenses(path=args.license_path)
     if args.useflags:
-        check_use_flags(path=args.use_path)
+        check_use_flags(path=args.use_path, check_sort_order=args.sort_order)
