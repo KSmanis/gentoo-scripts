51b2fa4f7a407afeadff4fd7c8964dc6e275b79e
Konstantinos Smanis <konstantinos.smanis@gmail.com>
2020-04-07 10:51:23 +0200

    Conform to PEP 8

--- check_packages.py
+++ check_packages.py
@@ -23,44 +23,134 @@ def _is_sorted(iterable, comparator=operator.le):
     return all(map(comparator, a, b))
 
 
-def _get_input_file_paths(path):
-    if path.is_dir():
-        return sorted(filter(lambda x: x.is_file() and not x.is_symlink(), path.iterdir()))
-    else:
-        return [path]
+def _log(message, **kwargs):
+    format_map = {
+        'file_name': portage.output.white,
+        'atom': portage.output.red,
+        'highlight': portage.output.yellow,
+    }
+    for k, v in kwargs.items():
+        if k in format_map:
+            kwargs[k] = format_map[k](v)
+    print(message.format(**kwargs))
 
 
 def _parse_args():
-    parser = argparse.ArgumentParser(description="Check package configuration.")
+    parser = argparse.ArgumentParser(
+        description="Check package configuration.",
+    )
     keyword_group = parser.add_mutually_exclusive_group()
-    keyword_group.add_argument('-k', '--no-keywords', dest='keywords', action='store_false',
-                               help="do not check package keywords")
-    keyword_group.add_argument('-K', '--keywords', dest='keywords', action='store_true',
-                               help="check package keywords (default)")
+    keyword_group.add_argument(
+        '-k', '--no-keywords',
+        dest='keywords',
+        action='store_false',
+        help="do not check package keywords",
+    )
+    keyword_group.add_argument(
+        '-K', '--keywords',
+        dest='keywords',
+        action='store_true',
+        help="check package keywords (default)",
+    )
     license_group = parser.add_mutually_exclusive_group()
-    license_group.add_argument('-l', '--no-licenses', dest='licenses', action='store_false',
-                               help="do not check package licenses")
-    license_group.add_argument('-L', '--licenses', dest='licenses', action='store_true',
-                               help="check package licenses (default)")
+    license_group.add_argument(
+        '-l', '--no-licenses',
+        dest='licenses',
+        action='store_false',
+        help="do not check package licenses",
+    )
+    license_group.add_argument(
+        '-L', '--licenses',
+        dest='licenses',
+        action='store_true',
+        help="check package licenses (default)",
+    )
     sort_order_group = parser.add_mutually_exclusive_group()
-    sort_order_group.add_argument('-s', '--no-sort-order', dest='sort_order', action='store_false',
-                                  help="do not check sort order")
-    sort_order_group.add_argument('-S', '--sort-order', dest='sort_order', action='store_true',
-                                  help="check sort order (default)")
+    sort_order_group.add_argument(
+        '-s', '--no-sort-order',
+        dest='sort_order',
+        action='store_false',
+        help="do not check sort order",
+    )
+    sort_order_group.add_argument(
+        '-S', '--sort-order',
+        dest='sort_order',
+        action='store_true',
+        help="check sort order (default)",
+    )
     use_flag_group = parser.add_mutually_exclusive_group()
-    use_flag_group.add_argument('-u', '--no-use-flags', dest='use_flags', action='store_false',
-                                help="do not check package USE flags")
-    use_flag_group.add_argument('-U', '--use-flags', dest='use_flags', action='store_true',
-                                help="check package USE flags (default)")
-    parser.add_argument('--keyword-path', metavar='PATH', default=DEFAULT_KEYWORD_PATH,
-                        help="the path (file or directory) to check for package keywords (default: %(default)s)")
-    parser.add_argument('--license-path', metavar='PATH', default=DEFAULT_LICENSE_PATH,
-                        help="the path (file or directory) to check for package licenses (default: %(default)s)")
-    parser.add_argument('--use-path', metavar='PATH', default=DEFAULT_USE_PATH,
-                        help="the path (file or directory) to check for package USE flags (default: %(default)s)")
-    parser.set_defaults(keywords=DEFAULT_CHECK_KEYWORDS, licenses=DEFAULT_CHECK_LICENSES,
-                        sort_order=DEFAULT_CHECK_SORT_ORDER, use_flags=DEFAULT_CHECK_USE_FLAGS)
+    use_flag_group.add_argument(
+        '-u', '--no-use-flags',
+        dest='use_flags',
+        action='store_false',
+        help="do not check package USE flags",
+    )
+    use_flag_group.add_argument(
+        '-U', '--use-flags',
+        dest='use_flags',
+        action='store_true',
+        help="check package USE flags (default)",
+    )
+    parser.add_argument(
+        '--keyword-path',
+        metavar='PATH',
+        default=DEFAULT_KEYWORD_PATH,
+        help="the path (file or directory) to check for package keywords"
+             " (default: %(default)s)",
+    )
+    parser.add_argument(
+        '--license-path',
+        metavar='PATH',
+        default=DEFAULT_LICENSE_PATH,
+        help="the path (file or directory) to check for package licenses"
+             " (default: %(default)s)",
+    )
+    parser.add_argument(
+        '--use-path',
+        metavar='PATH',
+        default=DEFAULT_USE_PATH,
+        help="the path (file or directory) to check for package USE flags"
+             " (default: %(default)s)",
+    )
+    parser.set_defaults(
+        keywords=DEFAULT_CHECK_KEYWORDS,
+        licenses=DEFAULT_CHECK_LICENSES,
+        sort_order=DEFAULT_CHECK_SORT_ORDER,
+        use_flags=DEFAULT_CHECK_USE_FLAGS,
+    )
     return parser.parse_args()
+
+
+def _portage_porttree_api():
+    return portage.db[portage.root]['porttree'].dbapi
+
+
+def _portage_unstable_arch():
+    return '~' + portage.settings['ARCH']
+
+
+def _portage_vartree_api():
+    return portage.db[portage.root]['vartree'].dbapi
+
+
+def _read_path(path):
+    for p in _resolve_path(path):
+        with open(p) as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith('#'):
+                    yield f, line
+
+
+def _resolve_path(path):
+    if path.is_dir():
+        filtered_dir_contents = filter(
+            lambda x: x.is_file() and not x.is_symlink(),
+            path.iterdir(),
+        )
+        return sorted(filtered_dir_contents)
+    else:
+        return [path]
 
 
 def _strip_use_flag(use_flag, prefix=('+', '-')):
@@ -69,105 +159,114 @@ def _strip_use_flag(use_flag, prefix=('+', '-')):
 
 def check_keywords(path=DEFAULT_KEYWORD_PATH):
     print("Checking keywords...")
-    for p in _get_input_file_paths(Path(path)):
-        with open(p) as f:
-            for line in f:
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
+    for file, line in _read_path(Path(path)):
+        tokens = line.split()
+        atom = tokens[0]
+        keywords = tokens[1:] or [_portage_unstable_arch()]
+        installed_packages = _portage_vartree_api().match(atom)
+        if not installed_packages:
+            _log(
+                "{file_name}: {atom} is no longer installed!",
+                file_name=file.name,
+                atom=atom,
+            )
+            continue
+        elif portage.versions.cpv_getversion(atom) is None:
+            continue
 
-                tokens = line.split()
-                atom = tokens[0]
-                keywords = tokens[1:] or ['~{host_arch}'.format(host_arch=portage.settings['ARCH'])]
-                installed_packages = portage.db[portage.root]['vartree'].dbapi.match(atom)
-                if not installed_packages:
-                    print("{file_name}: {atom} is no longer installed!".format(
-                        file_name=portage.output.white(f.name),
-                        atom=portage.output.red(atom),
-                    ))
-                    continue
-                elif portage.versions.cpv_getversion(atom) is None:
-                    continue
-
-                for package in installed_packages:
-                    try:
-                        package_keywords = portage.db[portage.root]['porttree'].dbapi.aux_get(package, ['KEYWORDS'])[0]
-                    except portage.exception.PortageKeyError:
-                        print("{file_name}: {atom} is no longer available in the portage tree!".format(
-                            file_name=portage.output.white(f.name),
-                            atom=portage.output.red(atom),
-                        ))
-                    else:
-                        available_keywords = package_keywords.split()
-                        for keyword in keywords:
-                            if keyword not in available_keywords:
-                                print("{file_name}: {atom} no longer requires the {keyword} keyword!".format(
-                                    file_name=portage.output.white(f.name),
-                                    atom=portage.output.red(atom),
-                                    keyword=portage.output.yellow(keyword),
-                                ))
+        for package in installed_packages:
+            try:
+                package_keywords = _portage_porttree_api().aux_get(
+                    package,
+                    ['KEYWORDS'],
+                )[0]
+            except portage.exception.PortageKeyError:
+                _log(
+                    "{file_name}: {atom} is no longer available in the portage"
+                    " tree!",
+                    file_name=file.name,
+                    atom=atom,
+                )
+            else:
+                available_keywords = package_keywords.split()
+                for keyword in keywords:
+                    if keyword not in available_keywords:
+                        _log(
+                            "{file_name}: {atom} no longer requires the"
+                            " {highlight} keyword!",
+                            file_name=file.name,
+                            atom=atom,
+                            highlight=keyword,
+                        )
 
 
 def check_licenses(path=DEFAULT_LICENSE_PATH):
     print("Checking licenses...")
-    for p in _get_input_file_paths(Path(path)):
-        with open(p) as f:
-            for line in f:
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-
-                atom = line.split(maxsplit=1)[0]
-                installed_packages = portage.db[portage.root]['vartree'].dbapi.match(atom)
-                if not installed_packages:
-                    print("{file_name}: {atom} is no longer installed!".format(
-                        file_name=portage.output.white(f.name),
-                        atom=portage.output.red(atom),
-                    ))
+    for file, line in _read_path(Path(path)):
+        atom = line.split(maxsplit=1)[0]
+        installed_packages = _portage_vartree_api().match(atom)
+        if not installed_packages:
+            _log(
+                "{file_name}: {atom} is no longer installed!",
+                file_name=file.name,
+                atom=atom,
+            )
 
 
-def check_use_flags(path=DEFAULT_USE_PATH, check_sort_order=DEFAULT_CHECK_SORT_ORDER):
+def check_use_flags(path=DEFAULT_USE_PATH,
+                    check_sort_order=DEFAULT_CHECK_SORT_ORDER):
     print("Checking USE flags...")
-    for p in _get_input_file_paths(Path(path)):
-        with open(p) as f:
-            for line in f:
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
+    for file, line in _read_path(Path(path)):
+        tokens = line.split()
+        atom = tokens[0]
+        use_flags = tokens[1:]
+        if check_sort_order:
+            stripped_use_flags = map(
+                partial(_strip_use_flag, prefix='+'),
+                use_flags,
+            )
+            if not _is_sorted(stripped_use_flags):
+                _log(
+                    "{file_name}: {atom} has unsorted USE flags!",
+                    file_name=file.name,
+                    atom=atom,
+                )
+        installed_packages = _portage_vartree_api().match(atom)
+        if not installed_packages:
+            _log(
+                "{file_name}: {atom} is no longer installed!",
+                file_name=file.name,
+                atom=atom,
+            )
+            continue
 
-                tokens = line.split()
-                atom = tokens[0]
-                use_flags = tokens[1:]
-                if check_sort_order and not _is_sorted(map(partial(_strip_use_flag, prefix='+'), use_flags)):
-                    print("{file_name}: {atom} has unsorted USE flags!".format(
-                        file_name=portage.output.white(f.name),
-                        atom=portage.output.red(atom),
-                    ))
-                installed_packages = portage.db[portage.root]['vartree'].dbapi.match(atom)
-                if not installed_packages:
-                    print("{file_name}: {atom} is no longer installed!".format(
-                        file_name=portage.output.white(f.name),
-                        atom=portage.output.red(atom),
-                    ))
-                    continue
-
-                for package in installed_packages:
-                    try:
-                        package_iuse = portage.db[portage.root]['porttree'].dbapi.aux_get(package, ['IUSE'])[0]
-                    except portage.exception.PortageKeyError:
-                        print("{file_name}: {atom} is no longer available in the portage tree!".format(
-                            file_name=portage.output.white(f.name),
-                            atom=portage.output.red(atom),
-                        ))
-                    else:
-                        available_use_flags = list(map(_strip_use_flag, package_iuse.split()))
-                        for use_flag in map(_strip_use_flag, use_flags):
-                            if use_flag not in available_use_flags:
-                                print("{file_name}: {atom} no longer makes use of the {use_flag} USE flag!".format(
-                                    file_name=portage.output.white(f.name),
-                                    atom=portage.output.red(atom),
-                                    use_flag=portage.output.yellow(use_flag),
-                                ))
+        for package in installed_packages:
+            try:
+                package_iuse = _portage_porttree_api().aux_get(
+                    package,
+                    ['IUSE'],
+                )[0]
+            except portage.exception.PortageKeyError:
+                _log(
+                    "{file_name}: {atom} is no longer available in the portage"
+                    " tree!",
+                    file_name=file.name,
+                    atom=atom,
+                )
+            else:
+                available_use_flags = list(map(
+                    _strip_use_flag,
+                    package_iuse.split(),
+                ))
+                for use_flag in map(_strip_use_flag, use_flags):
+                    if use_flag not in available_use_flags:
+                        _log(
+                            "{file_name}: {atom} no longer makes use of the"
+                            " {highlight} USE flag!",
+                            file_name=file.name,
+                            atom=atom,
+                            highlight=use_flag,
+                        )
 
 
 if __name__ == '__main__':
